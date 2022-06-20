34ecf79cde7dfdf1c78dcb898dd7320c4550abd4
Sofia Goncalves <21234118@shrewsbury.ac.uk>
2022-06-20 10:00:58 +0100

    hmmmmm

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace functions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hmm");
+            int[] arrayToSort = { 1, 5, 2, 4, 7};
+            InsertionSort(arrayToSort); 
+            
+        }
+        static void BubbleSort(int[] arrayToSort)
+        {
+            Console.WriteLine();
+        }
+        static void PrintList(int[]list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void InsertionSort(int[] arrayToSort)
+        {
+            for (int i = 0; i < arrayToSort.Length-1; i++){
+                int itemToSort = arrayToSort[i+1];
+                
+            }
         }
     }
 }
+
